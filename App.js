5bd9a36e14ae72ec98b7240b1b8526a95a5285e8
Gaurav Bhadauria <gauravbhadauria@Gauravs-MacBook-Pro.local>
2023-04-07 08:51:35 +0530

    changes

--- App.js
+++ App.js
@@ -1,117 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component, useEffect, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  Table,
+  TableWrapper,
+  Row,
+  Rows,
+  Col,
+} from 'react-native-table-component';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [tableHead, setTableHead] = useState([
+    'Users',
+    'Contact',
+    'Email',
+    'Address',
+  ]);
+  const [tableTitle, setTableTitle] = useState([
+    'gaurav',
+    'ankit',
+    'kabir',
+    'raj',
+  ]);
+  const [tableData, setTableData] = useState([
+    ['8888888383', 'gaurav@gmail.com', 'delhi'],
+    ['3847638883', 'ankit@gmail.com', 'Noida'],
+    ['9938373737', 'kabir@gamil.com', 'Jaipur'],
+    ['9373939334', 'raj@gmail.com', 'Mumbai'],
+  ]);
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Table borderStyle={{borderWidth: 1}}>
+        <Row
+          data={tableHead}
+          flexArr={[1, 2, 1, 1]}
+          style={styles.head}
+          textStyle={styles.text}
+        />
+        <TableWrapper style={styles.wrapper}>
+          <Col
+            data={tableTitle}
+            style={styles.title}
+            heightArr={[28, 28]}
+            textStyle={styles.text}
+          />
+          <Rows
+            data={tableData}
+            flexArr={[2, 1, 1]}
+            style={styles.row}
+            textStyle={styles.text}
+          />
+        </TableWrapper>
+      </Table>
     </View>
   );
 };
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+const styles = StyleSheet.create({
+  container: {flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff'},
+  head: {height: 40, backgroundColor: '#f1f8ff'},
+  wrapper: {flexDirection: 'row'},
+  title: {backgroundColor: '#f6f8fa'},
+  row: {},
+  text: {textAlign: 'center'},
+});
